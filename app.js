5973b8738b137fc509efa3557fd457e5a8538390
arinze <obilaurent54@gmail.com>
2021-01-15 11:39:32 +0100

    add new page twitter bookmark

--- app.js
+++ app.js
@@ -1,121 +1,52 @@
-function getRandomValue(max, min) {
-    return Math.floor(Math.random() * (max - min)) + min;
-}
-
-
-
 const app = Vue.createApp({
     data() {
-        return {
-            playerHealth: 100,
-            monsterHealth: 100,
-            currentRound: 0,
-            winner: null,
-            logMessages: []
-        }
-    },
-    computed: {
-        monsterHealthBarStyle() {
-
-            if(this.monsterHealth < 0) {
-
-               return { width: 0 + '%' }
-            }
-           return { width: this.monsterHealth + '%' }
-        },
-        playerHealthBarStyle() { 
-
-            if(this.playerHealth < 0) {
-                
-                return { width: 0 + '%' }
-            }
-           return  { width: this.playerHealth + '%' }
-        },
-        mayUseSpecialAttack () {
-
-            return this.currentRound % 3 !== 0 
-    }
-    },
-    watch: {
-        playerHealth(value) {
-            if(value <= 0 && this.monsterHealth <= 0) {
-                this.winner = 'Draw'
-                // a draw
-            } else if(value <= 0) {
-
-                this.winner = 'Monster'
-            }
-        },
-        monsterHealth(value) {
-            if(value <= 0 && this.playerHealth <= 0){
-                this.winner = 'Draw'
-            } else if(value < 0) {
-
-                this.winner = 'Player'
-            }
-        }
+      return {
+        maxCharactersLength: 280,
+        tweetCharacters: "",
+        remainingCharacters: 280,
+        liked: false,
+        tweets: [],
+        favTweets: [],
+        sourceImg: 'https://pbs.twimg.com/profile_images/1278577817014411264/wRS_3jPo_400x400.jpg'
+      };
     },
     methods: {
-        startNewGame() {
-            this.playerHealth  = 100
-            this.monsterHealth = 100
-            this.winner        = null
-            this.currentRound  = 0
-        },
-        attackMonster() {
-
-            this.currentRound++;
-            const attackValue  = getRandomValue(12, 5)
-            this.monsterHealth = this.monsterHealth - attackValue
-            this.attackPlayer();
-
-            this.addLogMessages('Player', 'Attacks', attackValue)
-
-        
-        },
-        attackPlayer() {
-
-            const attackValue  = getRandomValue(15, 8)
-            this.playerHealth  = this.playerHealth - attackValue
-
-            this.addLogMessages('Monster', 'Attacks', attackValue)
-
-            // this.healthStatus
-        },
-        specialAttackMonster() {
-
-            this.currentRound++;
-            const attackValue  = getRandomValue(25, 10);
-            this.monsterHealth = this.monsterHealth - attackValue;
-            this.attackPlayer()
-
-            this.addLogMessages('Player', 'Attacks', attackValue)
-            
-            // this.healthStatus
-        },
-        healPlayer() {
-            const healValue = getRandomValue(20, 8)
-            if(this.playerHealth + healValue > 100) {
-                this.playerHealth = 100
-            } else {
-                this.playerHealth = this.playerHealth + healValue
-            }
-
-            this.attackPlayer()
-            this.addLogMessages('Player', 'Heals', healValue)
-        },
-        surrender() {
-            this.winner = 'Monster'
-        },
-        addLogMessages(who, what, value) {
-            this.logMessages.unshift({
-                actionBy: who,
-                actionType: what,
-                actionValue: value
-            })
+      like() {
+        this.liked = !this.liked;
+      },
+      addTweet() {
+          const id       = new Date().toISOString()
+          const newTweet = {
+              value: this.tweetCharacters,
+              id: id
+          }
+          this.tweets.push(newTweet)
+          this.tweetCharacters = ''
+      },
+      removeItem(event) {
+          const tweetId = event.path[4].id
+          this.tweets = this.tweets.filter((tweet) => tweet.id !== tweetId)
+      }
+    },
+    computed: {
+      checkTweetLength() {
+        if (this.remainingCharacters === 0) {
+          return;
+        } else {
+          return (this.remainingCharacters =
+            this.maxCharactersLength - this.tweetCharacters.length);
         }
-
-    }
-})
-
-app.mount('#game')
+      },
+      getTimestamp() {
+        const day = String(new Date());
+        const stringDay = day.split("");
+        const returnDay = stringDay.splice(4, 7).join("");
+  
+        return returnDay;
+      },
+    },
+  });
+  
+  app.mount("#app");
+  
+  
